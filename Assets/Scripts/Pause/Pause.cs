23315e13fb52322dad0f0a4610708d033d2d2d24
João Victor Duarte <joaodicu387@gmail.com>
2022-01-31 03:10:02 -0300

    Implementando pause no jogo.

---
+++ Assets/Scripts/Pause/Pause.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using EndlessRunner.ScriptableObjects.Events;
+
+namespace EndlessRunner.Pause
+{
+    public sealed class Pause : MonoBehaviour
+    {
+        [SerializeField] private GameEvent onGamePaused = default;
+        [SerializeField] private GameEvent onResumeGame = default;
+
+        private void OnEnable() 
+        {
+            onGamePaused.onGameListener += PauseGame;
+            onResumeGame.onGameListener += ResumeGame;
+        }
+
+        private void OnDisable() 
+        {
+            onGamePaused.onGameListener -= PauseGame;
+            onResumeGame.onGameListener -= ResumeGame;
+        }
+
+        private void PauseGame()
+        {
+            Time.timeScale = 0;
+        }
+
+        private void ResumeGame()
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Pause/UI_Pause.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using EndlessRunner.ScriptableObjects.Events;
+
+namespace EndlessRunner.UI.Pause
+{
+    public sealed class UI_Pause : MonoBehaviour
+    {
+        [SerializeField] private GameEvent onGamePaused = default;
+        [SerializeField] private GameEvent onResumeGame = default;
+        [SerializeField] private GameObject panelPause;
+
+        private void Awake()
+        {
+            panelPause.SetActive(true);
+
+            Button pauseButton = GameObject.FindGameObjectWithTag("pauseButton").GetComponent<Button>();
+            pauseButton.onClick.AddListener(PauseGame);
+
+            Button resumeButton = GameObject.FindGameObjectWithTag("resumeButton").GetComponent<Button>();
+            resumeButton.onClick.AddListener(ResumeGame);
+
+            panelPause.SetActive(false);
+        }
+
+        private void PauseGame() 
+        {
+            panelPause.SetActive(true);
+            onGamePaused.Raise();
+        }
+
+        private void ResumeGame()
+        {
+            panelPause.SetActive(false);
+            onResumeGame.Raise();
+        }
+    }
+}
